e21ed6a14adf8bf6f7aa28437a6ddb09d3d1731a
Nicolas Barrios <nbarrios1337@gmail.com>
2019-06-11 10:39:10 -0400

    Update pop-post-install.sh
    Fixed multiple add-apt-repository error. 
    Added a few more packages to install, namely pop-desktop and nvme-cli.
    Fixed spotify install.

--- pop-post-install.sh
+++ pop-post-install.sh
@@ -2,12 +2,14 @@
 
 # Remember to 'chmod +x' this file
 
-add-apt-repository universe restricted multiverse
-sudo add-apt-repository ppa:daniruiz/flat-remix
+add-apt-repository universe 
+add-apt-repository restricted 
+add-apt-repository multiverse
+add-apt-repository ppa:daniruiz/flat-remix
 apt update
 apt full-upgrade -y
 apt install -y \
-powertop neofetch vlc git hub steam \
+powertop neofetch vlc git hub steam nvme-cli\
 flat-remix-gtk flat-remix-gnome flat-remix \
 ubuntu-restricted-extras \
 flatpak gnome-software-plugin-flatpak \
@@ -15,13 +17,15 @@ gnome-tweak-tool gnome-tweaks gnome-shell-extensions
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-snap://spotify
+snap install spotify
 
 
 #Final Steps
 apt autoclean -y
 apt autoremove -y
+apt install pop-desktop sessioninstaller
 
+#TODO can't be root here
 firefox \
 https://www.google.com/chrome/browser/desktop/index.html \
 https://support.system76.com/ \
