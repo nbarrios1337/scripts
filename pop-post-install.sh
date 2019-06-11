b54a6d0d384d0712b41c72429167e2dc482259a0
Nicolas Barrios <nbarrios1337@gmail.com>
2019-06-11 11:49:41 -0400

    Update pop-post-install.sh
    Fixed firefox error when running it as root.
    Removed unnecessary ubuntu-restricted-extras & git package.
    Forced confirmation of flatpak remote-add.

--- pop-post-install.sh
+++ pop-post-install.sh
@@ -9,13 +9,12 @@ add-apt-repository ppa:daniruiz/flat-remix
 apt update
 apt full-upgrade -y
 apt install -y \
-powertop neofetch vlc git hub steam nvme-cli\
+powertop neofetch vlc hub steam nvme-cli\
 flat-remix-gtk flat-remix-gnome flat-remix \
-ubuntu-restricted-extras \
 flatpak gnome-software-plugin-flatpak \
 gnome-tweak-tool gnome-tweaks gnome-shell-extensions
 
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add -y --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 snap install spotify
 
@@ -23,10 +22,9 @@ snap install spotify
 #Final Steps
 apt autoclean -y
 apt autoremove -y
-apt install pop-desktop sessioninstaller
+apt install -y pop-desktop sessioninstaller
 
-#TODO can't be root here
-firefox \
+sudo -u $USER firefox \
 https://www.google.com/chrome/browser/desktop/index.html \
 https://support.system76.com/ \
 https://extensions.gnome.org/ \
