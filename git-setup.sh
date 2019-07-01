de39e93a562691c7822382ca02c2d4f1780608f1
Nicolas Barrios <nbarrios1337@gmail.com>
2019-06-30 20:33:45 -0400

    Added logging capability to check if git-setup.sh ever runs

--- git-setup.sh
+++ git-setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-read -r -p "Set your git username" username
-git config --global user.name "$username"
-read -r -p "Set your git email" email
-git config --global user.name "$email"
+log_directory=/var/log/scripts/
+#Added logging capability to check if script ever runs
+{
+    read -r -p "Set your git username" username
+    git config --global user.name "$username"
+    read -r -p "Set your git email" email
+    git config --global user.name "$email"
+} 2>&1 | sudo tee "${log_directory}""$(basename "$0")".log
+
+echo "$0": Log of the script execution stored at: "${log_directory}""$(basename "$0")".log
