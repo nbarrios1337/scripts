4d864c500eaa0820d8b78e37e4c4cd1238920197
Nicolas Barrios <nbarrios1337@gmail.com>
2019-06-30 20:30:25 -0400

    Major bugfixes after latest test (candy.sh)
    Attmpted a symlink fix by creating the destination directory beforehand.
    Added rm commands to remove tmatrix's extracted installation.tar.gz.
    Added -nv argument to all wget calls to block progress bar/updates appearing in the log files.
    Fixed incorrect cht.sh alias for .bashrc.

--- candy.sh
+++ candy.sh
@@ -4,6 +4,9 @@ log_directory=/var/log/scripts/
 
 {
     cp ~/.bashrc ~/.bashrc.backup
+
+    #symlink command fails, therefore:
+    mkdir -p ~/.local/bin/bonsai
     #Bonsai.sh steup and install (Copied from https://gitlab.com/jallbrit/bonsai.sh/blob/master/README.md#manual-install)
     git clone https://gitlab.com/jallbrit/bonsai.sh ~/bin/bonsai.sh
     chmod +x ~/bin/bonsai.sh/bonsai.sh
@@ -12,17 +15,18 @@ log_directory=/var/log/scripts/
 
     #TMatrix  setup and install (Copied from https://github.com/M4444/TMatrix/blob/master/README.md#download-and-install-on-other-gnulinux-distributions)
     sudo apt-get install -y libncurses5
-    wget -q https://github.com/M4444/TMatrix/releases/download/v1.1/installation.tar.gz
+    wget -nv https://github.com/M4444/TMatrix/releases/download/v1.1/installation.tar.gz
     tar -zxvf installation.tar.gz
-    cd installation || (echo "TMatrix extract failed." && exit)
-    sudo ./install.sh
+    #script won't run unless extract was successful
+    (cd installation || echo "TMatrix extract failed.") && sudo ./install.sh
+    rm -r ./installation*
 
     #cheat.sh client setup and install (Copied from https://github.com/chubin/cheat.sh#installation)
     sudo apt-get install -y rlwrap
     mkdir -p ~/bin/
     curl https://cht.sh/:cht.sh >~/bin/cht.sh
     chmod +x ~/bin/cht.sh
-    echo "alias cheat='cht.sh'" >>~/.bashrc
+    echo "alias cheat='~/bin/cht.sh'" >>~/.bashrc
 
     #one call to reload the aliases
     source ~/.bashrc
